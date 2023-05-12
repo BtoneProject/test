b20d72b8fc0c45aeb9010a275a73267e011117d5
beroso20th <beroso20th@gmail.com>
2023-05-12 11:02:06 +0900

    깃 연동 테스트

---
+++ src/main/java/com/example/btonepractice/BtonePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.btonepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtonePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtonePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/btonepractice/BtonePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.btonepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtonePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
